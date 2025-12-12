062b4feca84886445d63eff5b3185c13f81b8a8d
Shrink <tq4zwzvn45@privaterelay.appleid.com>
2025-12-12 11:30:22 -0600

    Changing granularity to 2min, with a 30d range

--- script.mjs
+++ script.mjs
@@ -396,8 +396,8 @@ function populate_symbols (/** @type {HTMLElement} */ element, /** @type {symbol
 async function fetch_quotes (/** @type {string} */ symbol) {
 	let proxy_url = decodeURIComponent('%68%74%74%70%73%3A%2F%2F%64%65%76%65%6C%2E%73%65%61%6E%64%75%6E%61%77%61%79%2E%63%6F%6D%2F%38%38%38%38%2F')
 	let base_url = 'https://query1.finance.yahoo.com/v8/finance/chart/'
-	let interval = '1m'
-	let range = '5d'
+	let interval = '2m'
+	let range = '30d'
 
 	let url = `${proxy_url}${base_url}${symbol}=f?&interval=${interval}&range=${range}`
 
